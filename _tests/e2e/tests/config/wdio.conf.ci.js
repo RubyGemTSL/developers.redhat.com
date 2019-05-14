5d8745f5f730f8ec15f3ea1625263d9e8ffdad34
Ian Hamilton <ian.hamilton@dnata.com>
2019-05-14 20:12:02 +0400

    MWES-3086: Initial commit of having e2e-tests use MP manager test base image
    wip
    
    wip
    
    wip
    
    Tests are running using blessed image
    
    Mount download directory and copy test report
    
    MWES-3086: Initial commit of having e2e-tests use MP manager test base image
    
    wip
    
    wip
    
    wip
    
    Tests are running using blessed image
    
    Merge remote-tracking branch 'origin/MWES-3086' into MWES-3086
    
    MWES-3086: Initial commit of having e2e-tests use MP manager test base image
    
    wip
    
    wip
    
    wip
    
    Tests are running using blessed image
    
    Mount download directory and copy test report
    
    MWES-3086: Initial commit of having e2e-tests use MP manager test base image
    
    wip
    
    wip
    
    wip
    
    Tests are running using blessed image
    
    Merge remote-tracking branch 'origin/MWES-3086' into MWES-3086
    
    Merge remote-tracking branch 'origin/MWES-3086' into MWES-3086
    
    MWES-3086: Initial commit of having e2e-tests use MP manager test base image
    
    wip
    
    wip
    
    wip
    
    Tests are running using blessed image
    
    Mount download directory and copy test report
    
    MWES-3086: Initial commit of having e2e-tests use MP manager test base image
    
    wip
    
    wip
    
    wip
    
    Tests are running using blessed image
    
    Merge remote-tracking branch 'origin/MWES-3086' into MWES-3086
    
    Merge remote-tracking branch 'origin/MWES-3086' into MWES-3086
    
    test fixes
    
    test fixes
    
    wip for Rob
    
    removed test tag
    
    updated ruby files for MP changes
    
    removed unit-tests
    
    Added user to compose file in order to keep permissions in sync for copying things out of the container when archiving the artifacts
    
    addressed rob's comments (from slack)
    
    Ignore drupal test for mobile
    
    Attempting to fix report creation.
    
    [WIP] Further steps to try and get downloads working for headless chrome
    
    Test fixes
    
    removed env vars
    
    Fixed downloads tests for headless chrome
    
    updated password
    
    fixed proxy error
    
    cleanup of tests
    
    cleanup of tests
    
    fixes for failing tests
    
    Update docker-compose.yml
    
    Added shm_size to try and fix chrome crashes.
    
    removed previous push
    
    wip
    
    fix for chrome crashes (hopefully)
    
    Update docker-compose.yml
    
    Increasing shm size to avoid chrome crashes.
    
    Update docker-compose.yml
    
    reformat
    
    fixed merge duplicate line
    
    re-added missing ARGS so no need to change Jenkins job in order to test this PR
    
    downgraded docker-compose version as 2.2 is unsupported See build #3713
    
    fixed mobile view tests
    
    fixed mobile search test
    
    refactored/cleaned up tests
    
    fixed rebase file path errors
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    cleaned up tests
    
    cleaned up tests
    
    ignore files
    
    only start chrome service for e2e tests
    
    fixed typo
    
    minor update: corrected baseUrl parameter for e2e tests.
    
    removed duplicate spec
    
    updated cheatsheet tests
    
    fixed download tests
    
    turn debugger on
    
    initial stab at internal selenium grid

--- _tests/e2e/tests/config/wdio.conf.ci.js
+++ _tests/e2e/tests/config/wdio.conf.ci.js
@@ -1,11 +1,15 @@
 const commandlineArgs = require('minimist')(process.argv.slice(2));
 const base = require('./wdio.conf.base.js').config;
 const Browser = require('../specs/support/Browser.Manager');
-const browserCaps = new Browser(commandlineArgs['browser'], commandlineArgs['headless']).create();
-const gridHost = typeof process.env.GRID_HOST === 'undefined' ? 'localhost' : process.env.GRID_HOST;
+const browserCaps = new Browser(commandlineArgs.browser).create();
+// const gridHost = typeof process.env.GRID_HOST === 'undefined' ? 'localhost' : process.env.GRID_HOST;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
 
 const dockerConfig = Object.assign(base, {
-    hostname: gridHost,
+    protocol: 'https',
+    hostname: 'developer-selenium-hub.ext.us-east.aws.preprod.paas.redhat.com',
+    port: 443,
+    path: '/wd/hub',
     capabilities: [browserCaps],
     maxInstances: 10,
 });
