f80885c1deb5da25daa5b2fe6188ab7a9bdad24a
Ian Hamilton <ian.hamilton@dnata.com>
2019-06-18 22:18:33 +0400

    Merge branch 'MWES-3086' of https://github.com/RubyGemTSL/developers.redhat.com into MWES-3086
    # Conflicts:
    #	_tests/e2e/environments/docker-compose.yml
    #	_tests/e2e/package-lock.json
    #	_tests/e2e/package.json
    #	_tests/e2e/tests/config/wdio.conf.base.js
    #	_tests/e2e/tests/config/wdio.conf.js
    #	_tests/e2e/tests/specs/rhd-404-error.spec.js
    #	_tests/e2e/tests/specs/rhd-downloads.spec.js
    #	_tests/e2e/tests/specs/rhd-general-error.spec.js
    #	_tests/e2e/tests/specs/rhd-navigation-spec.js
    #	_tests/e2e/tests/specs/rhd-search.spec.js
    #	_tests/e2e/tests/specs/support/Browser.Manager.js
    #	_tests/e2e/tests/specs/support/pages/Drupal.Login.page.js
    #	_tests/e2e/tests/specs/support/pages/Page.js
    #	_tests/e2e/tests/specs/support/pages/ProductOverview.page.js
    #	_tests/e2e/tests/specs/support/pages/drupal/Config.page.js
    #	_tests/e2e/tests/specs/support/utils/Driver.Extension.js
    #	_tests/node_jenkins_test_runner.rb
    #	_tests/run_test_options.rb

---
+++ _tests/e2e/tests/config/wdio.conf.js
@@ -0,0 +1,11 @@
+const commandlineArgs = require('minimist')(process.argv.slice(2));
+const base = require('./wdio.conf.base.js').config;
+const Browser = require('../specs/support/Browser.Manager');
+
+const browserCaps = new Browser(commandlineArgs.browser).create();
+const localConfig = Object.assign(base, {
+    services: ['selenium-standalone'],
+    capabilities: [browserCaps],
+});
+
+exports.config = localConfig;
